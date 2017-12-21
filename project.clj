87fd45c8388df48cbfe19d5a46fcd8961da10651
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-12-21 21:27:34 +0100

    Playing with routes and resources

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
 (defproject musetrap-clj "0.1.0-SNAPSHOT"
+  :plugins [[lein-ring "0.12.2"]]
+  :ring {:handler musetrap-clj.core/handler}
   :description "Tool to provide prompts to ignite your inspiration"
   :url "https://github.com/excentris/musetrap-clj"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [liberator "0.15.1"]
+                 [compojure "1.6.0"]
+                 [ring/ring-core "1.6.3"]]
   :main ^:skip-aot musetrap-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/musetrap_clj/core.clj
+++ src/musetrap_clj/core.clj
@@ -1,5 +1,44 @@
 (ns musetrap-clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]]))
+
+(def data {
+  :recipes {:animal_warrior [animals weapons]
+            :humanoid_creature [creatures weapons]}
+  :bundles {:animals ["dog" "cat" "bird" "horse"]
+            :creatures ["ork" "werewolf" "troll" "dragon" "goblin"]
+            :weapons ["sword" "axe" "war hammer" "rifle" "pistol"]}})
+
+(defn get-ingredient
+  [bundle]
+  (take 1 (repeatedly #(rand-nth bundle))))
+
+(defn get-recipe
+  [recipe_id]
+  (get-in data [:recipes (keyword recipe_id)]))
+
+(defn cook-recipe
+  [recipe_id]
+  (flatten (map get-ingredient (get-recipe recipe_id))))
+
+(defresource recipe [recipe_id]
+  :available-media-types  ["application/json"]
+  :handle-ok (cook-recipe recipe_id))
+
+(defresource recipes []
+  :available-media-types  ["application/json"]
+  :handle-ok (get-in data [:recipes]))
+
+(defroutes app
+  (ANY "/recipes/:recipe_id" [recipe_id] (recipe recipe_id))
+  (ANY "/" [] (str "hello"))
+  (ANY "/recipes" [] (recipes)))
+
+(def handler 
+  (-> app 
+      wrap-params))
 
 (defn -main
   "I don't do a whole lot ... yet."
