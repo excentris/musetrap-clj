6412ef9059243dd31de8f85e22c5a36c35a66aeb
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2017-12-22 15:55:21 +0100

    Add multi parameter route

--- src/musetrap_clj/core.clj
+++ src/musetrap_clj/core.clj
@@ -23,9 +23,21 @@
   [recipe_id]
   (flatten (map get-ingredient (get-recipe recipe_id))))
 
+;; TODO this function must handle both recipe and bundle params
+(defn cook-improvised-recipe
+  [params]
+  ;; recipe can be either a single recipe_id or a vector of recipe_id
+  (if (vector? (get-in params ["recipe"]))
+    (concat (map cook-recipe (get-in params ["recipe"])))
+    (cook-recipe (get-in params ["recipe"]))))
+
 (defresource recipe [recipe_id]
   :available-media-types  ["application/json"]
   :handle-ok (cook-recipe recipe_id))
+
+(defresource atelier [params]
+  :available-media-types  ["application/json"]
+  :handle-ok (cook-improvised-recipe params))
 
 (defresource recipes []
   :available-media-types  ["application/json"]
@@ -34,6 +46,7 @@
 (defroutes app
   (ANY "/recipes/:recipe_id" [recipe_id] (recipe recipe_id))
   (ANY "/" [] (str "hello"))
+  (ANY "/cook" [& params] (atelier params))
   (ANY "/recipes" [] (recipes)))
 
 (def handler 
