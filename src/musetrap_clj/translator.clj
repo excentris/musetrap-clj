b27e9fc33ebd1750ae5a1e5caeb18a0c298c4c64
Eduardo Rubio <eduardo.rubiopaniza@sqills.com>
2018-01-30 22:17:56 +0100

    Move dictionary to edn file under resources/i18n

--- src/musetrap_clj/translator.clj
+++ src/musetrap_clj/translator.clj
@@ -1,67 +1,8 @@
 (ns musetrap-clj.translator
-  (:gen-class))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
-(def ^:private dictionary 
-  { 
-   :recipes {
-             :animal_warrior {
-                              :name {
-                                     :en "Animal Warrior"
-                                     :es "Guerrero animal" } 
-                              :description {
-                                            :en "An animal warrior"
-                                            :es "Un guerrero animal" }}
-             :humanoid_creature {
-                                 :name {
-                                        :en "Humanoid Creature"
-                                        :es "Criatura humanoide" }
-                                 :description {
-                                               :en "A humanoid creature"
-                                               :es "Una criatura humanoide" }}} 
-   :bundles {
-             :animals {
-                       :name {
-                              :en "Animals"
-                              :es "Animales" }}
-             :creatures {
-                         :name {
-                                :en "Creatures"
-                                :es "Criaturas" }}}
-   :ingredients {
-                 :dog {
-                       :name {
-                              :en "Dog"
-                              :es "Perro" }}
-                 :cat {
-                       :name {
-                              :en "Cat"
-                              :es "Gato" }}
-                 :bird {
-                        :name {
-                               :en "Bird"
-                               :es "Pájaro" }}
-                 :horse {
-                         :name {
-                                :en "Horse"
-                                :es "Caballo" }}
-                 :ork {
-                       :name {
-                              :en "Ork"
-                              :es "Orco" }}
-                 :werewolf {
-                            :name {
-                                   :en "Werewolf"
-                                   :es "Hombre lobo" }}
-                 :troll {
-                         :name {
-                                :en "Troll"
-                                :es "Trol" }}
-                 :dragon {
-                          :name {
-                                 :en "Dragon"
-                                 :es "Dragón" }}
-                 } 
-   })
+(def ^:private dictionary (edn/read-string (slurp (io/resource "i18n/dictionary.edn"))))
 
 (defn translate
   [query default]
